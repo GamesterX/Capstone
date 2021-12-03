810e8545de076ff69f1d620cf5641bfa059a4d9c
rjaconski <rjaconski1@gmail.com>
2021-12-02 21:37:10 -0500

    Update movie.php

--- movie.php
+++ movie.php
@@ -1,3 +1,7 @@
+<!--movie page, takes the movie title the user selected on the homepage and uses it 
+to grab data from the database and the page is updated with the selected movie information.
+the movie name is used to select the right column-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,26 +31,17 @@
      $length = $contact[2];
      $id = $contact[3];
      $pop = $contact[4];
+     $syn = $contact[5];
      mysqli_free_result($result);
 db_close($conn);
     echo '
-<h1><a href="file:///C:\Programs\Home.html"><strong>Home</strong></a></h1>
 <h1> '. $title . ' </h1>
 <p>Release Date: '.$release.'</p>
 <p>Length: '. $length .'</p>
 <p>Popularity: '. $pop .'</p>
-<p>Synopsis:</p>
-<p>TMDB ID:<a href="http://www.themoviedb.org/movie/'.$id.'-the-movie-3">'.$id.'</a></p>
-<p>IMDB ID:<a href="http://www.imdb.com/title/tt13544716/">tt13544716 </a></p>';
+<p>Synopsis: '. $syn . '</p>
+<p>TMDB ID:<a href="http://www.themoviedb.org/movie/'.$id.'-the-movie-3">'.$id.'</a></p>';
 ?>
-<ul>
-<li><a href="file:///C:\Users\Sam\Documents\GitHub\Capstone\Dune.html">Dune</a></li>
-<li><a href="file:///C:\Users\Sam\Documents\GitHub\Capstone\Clifford.html">Clifford the Big Red Dog</a></li>
-<li><a href="file:///C:\Users\Sam\Documents\GitHub\Capstone\venom.html">Venom: Let There Be Carnage</a></li>
-<li><a href="file:///C:\Users\Sam\Documents\GitHub\Capstone\Eternals.html">Eternals</a></li>
-<li><a href="file:///C:\Users\Sam\Documents\GitHub\Capstone\Spencer.html">Spencer</a></li>
-<li><a href="file:///C:\Users\Sam\Documents\GitHub\Capstone\Soho.html">Last Night in Soho</a></li>
-<li><a href="file:///C:\Users\Sam\Documents\GitHub\Capstone\MyHero.html">My Hero Academia: World Heroes Mission</a></li>
-</ul>;
+<button onclick="document.location='Home.html'">Return Home</button>
 </body>
 </html>
